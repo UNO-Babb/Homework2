3a9a3a0143482d6b17ba9e43e4525c9ce8e73cdd
Derek Babb <dvbabb@unomaha.edu>
2020-09-21 09:25:29 -0500

    Updated indentation.

--- Email_Extractor.py
+++ Email_Extractor.py
@@ -7,18 +7,17 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 def main():
+  #prompt the user for a webpage url
+  url = 'https://www.unomaha.edu/college-of-information-science-and-technology/about/faculty-staff/index.php'
+  url_html = urllib.request.urlopen(url)
 
-    #prompt the user for a webpage url
-    url = 'https://www.unomaha.edu/college-of-information-science-and-technology/about/faculty-staff/index.php'
-    url_html = urllib.request.urlopen(url)
+  soup = BeautifulSoup(url_html, "html.parser")
 
-    soup = BeautifulSoup(url_html, "html.parser")
+  #Convert the soup to a string version
+  soup = str(soup)
 
-    #Convert the soup to a string version
-    soup = str(soup)
-
-    #Split the text into a list, with the end of line \n as the delimiter
-    for line in soup.split("\n"):
-        print(line)
+  #Split the text into a list, with the end of line \n as the delimiter
+  for line in soup.split("\n"):
+    print(line)
 
 main()
